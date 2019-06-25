077a73e509e6240bb3ae28ddc863b5455e9c1396
meenatchimadurai <meenakshinittrichy@gmail.com>
2019-06-25 23:30:22 +0530

    php files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include_once 'header.php';
+?>
+<section class="main-container">
+    <div class="main-wrapper">
+      <h2>home</h2>
+        <?php
+       // if(isset($_SESSION['u_id'])){
+          //  echo "You are logged in";
+       // }
+       // ?>
+
+    </div>
+</section>
+
+    
+</body>
+</html>
